683f7f1bff847c3ff91239d46fe2889384064643
halim <abdelhalimkou1@gmail.com>
2026-05-09 20:25:47 +0300

    ui home page

---
+++ default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.UI;
+
+namespace MediCare
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnPatientSignUp_Click(object sender, EventArgs e)
+        {
+            // Patient signup logic later
+        }
+
+        protected void btnDoctorSignUp_Click(object sender, EventArgs e)
+        {
+            // Doctor signup logic later
+        }
+    }
+}
